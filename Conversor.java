a8ad486259b565b6eb2a863f359e963c793cb290
alessandro042 <20223tn058@utez.edu.mx>
2025-06-19 16:43:50 -0600

    [ADD] Conversor funcional

---
+++ Conversor.java
@@ -0,0 +1,50 @@
+//codigo del conversor de unidades de medida Kilómetros a millas
+//Litros a galones
+//Gramos a onzas
+//Centímetros a pulgadas por consola 
+import java.util.Scanner;
+
+public class Conversor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Seleccione la conversión:");
+        System.out.println("1. Kilómetros a millas");
+        System.out.println("2. Litros a galones");
+        System.out.println("3. Gramos a onzas");
+        System.out.println("4. Centímetros a pulgadas");
+        
+        int opcion = scanner.nextInt();
+        
+        switch (opcion) {
+            case 1:
+                System.out.print("Ingrese kilómetros: ");
+                double km = scanner.nextDouble();
+                double millas = km * 0.621371;
+                System.out.println(km + " kilómetros son " + millas + " millas.");
+                break;
+            case 2:
+                System.out.print("Ingrese litros: ");
+                double litros = scanner.nextDouble();
+                double galones = litros * 0.264172;
+                System.out.println(litros + " litros son " + galones + " galones.");
+                break;
+            case 3:
+                System.out.print("Ingrese gramos: ");
+                double gramos = scanner.nextDouble();
+                double onzas = gramos * 0.035274;
+                System.out.println(gramos + " gramos son " + onzas + " onzas.");
+                break;
+            case 4:
+                System.out.print("Ingrese centímetros: ");
+                double centimetros = scanner.nextDouble();
+                double pulgadas = centimetros * 0.393701;
+                System.out.println(centimetros + " centímetros son " + pulgadas + " pulgadas.");
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
+        
+        scanner.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+el proposito de este proyecto es hacer un sistema mediante consola que permita hacer conversiones de mediciones 
+La tecnologia que se utlizo fue java
+SE corre el programa y despues se introduce el numero de la conversion que se desea hacer para posteriormente introducir la cantidad a convertir
+
